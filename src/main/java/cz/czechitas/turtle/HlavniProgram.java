8cce264062ee804f03257e41a189e2158b4fb880
HanaMartinkovaKuchynkova <h.m.kuchynkova@gmail.com>
2024-02-03 23:00:14 +0100

    Zelva kresli ctverec, obdelnik, kolecko a rovnostranny trojuhelnik.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -11,6 +11,73 @@ public class HlavniProgram {
 
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnRight(90);
+
+        nakresliCtverec(50);
+
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.turnLeft(90);
+
+        nakresliObdelnik(50, 100);
+
+        zofka.turnRight(90);
+        zofka.move(120);
+        zofka.turnLeft(90);
+        
+        nakresliKolecko(5);
+
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+
+        zofka.turnRight(30);
+        
+        nakresliRovnostrannyTroj(50);
+
+        zofka.turnLeft(30);
+    }
+
+    private void nakresliRovnostrannyTroj(int delkaStrTroj) {
+        zofka.penDown();
+        for (int i = 0; i < 3; i++) {
+            zofka.move(delkaStrTroj);
+            zofka.turnRight(120);
+        }
+        zofka.penUp();
+    }
+
+    private void nakresliKolecko(int delkaStrKolecka) {
+        zofka.penDown();
+        for (int i = 0; i < 48; i++) {
+            zofka.move(delkaStrKolecka);
+            zofka.turnRight(7.5);
+        }
+        zofka.penUp();
+    }
+
+    private void nakresliObdelnik(int delkaStrA, int delkaStrB) {
+        zofka.penDown();
+        for (int i = 0; i < 2; i++) {
+            zofka.move(delkaStrA);
+            zofka.turnRight(90);
+            zofka.move(delkaStrB);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+    }
+
+    private void nakresliCtverec(int delkaStrCtverce) {
+        zofka.penDown();
+        for (int i = 0; i < 4; i++) {
+            zofka.move(delkaStrCtverce);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
     }
 
 }
